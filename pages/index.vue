d6d64534301433d57ae4d407b1a59d56d7092d63
s10nishka <rockslon2@gmail.com>
2022-12-12 19:27:37 +0200

    add lodash, import state to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <Tutorial/>
+  <div>#1. {{ topAlbum.name }}</div>
 </template>
 
 <script>
+  import { mapState } from "vuex"
+  import _ from "lodash"
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  computed: {
+    content() {
+      return [
+        this.topAlbum,
+        _.filter(this.alboms, {'type': 'new'}),
+        [ this.teasers[0], this.teasers[1] ],
+
+      ]
+    },
+    ...mapState({
+      topAlbum: state => state.topAlbum,
+      teasers: state => state.teasers,
+      alboms: state => state.alboms
+    })
+  }
 }
 </script>
